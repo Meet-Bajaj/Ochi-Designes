91cea73a01078c08a17eacfc93afc1ba2e312ebb
Meet-Bajaj <meetbajaj005@gmail.com>
2024-10-03 10:14:00 +0530

    Update listot3

--- my-app/src/components/listfot3.js
+++ my-app/src/components/listfot3.js
@@ -2,9 +2,9 @@ export default function List3(props){
     return (
         <>
         <div className="flex justify-between py-4 px-16 border border-gray-600 border-r-0 border-s-0 ">
-        <span className="text-xl underline">{props.one}</span>
-        <span className="text-xl ">{props.two}</span>
-        <span className="text-xl underline">READ</span>
+        <span className="text-xl w-40 underline">{props.one}</span>
+        <span className="text-xl w-40">{props.two}</span>
+        <span className="text-xl w-40 underline">READ</span>
         </div>
         </>
     );

84b9a012f1e18996df61fd4dfaca9e2612404dd4
Meet-Bajaj <meetbajaj005@gmail.com>
2024-10-04 19:44:37 +0530

    Updated App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
-
+import Second from './components/2';
+import THIRD from './components/3';
+import Footer from "./components/footer"
+import NAV from './components/header';
+import MAIN from './components/main'
 function App() {
   return (
-    <></>
+    <>
+    <NAV/>
+    <MAIN/>
+    <Second/>
+    <THIRD/>
+    <Footer/>
+    </>
   );
 }
 
